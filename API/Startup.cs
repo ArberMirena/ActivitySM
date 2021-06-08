4a4a30e1fe7c63c974ddd2805633bd3ff1e3b42a
ArberMirena <Arber@DESKTOP-THEPAPA>
2021-06-08 03:11:18 +0200

    Added react project and linked with c# via API

--- API/Startup.cs
+++ API/Startup.cs
@@ -42,7 +42,15 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
 
+            });
 
         }
 
@@ -59,6 +67,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
