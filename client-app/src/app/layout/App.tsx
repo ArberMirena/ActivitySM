6260c7cbf9096bf4fc2482acc174a74fdb5a8e36
ArberMirena <arbermirenaa@gmail.com>
2021-06-10 23:31:44 +0200

    Finished CRUD UI in React

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,13 +1,14 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, Header, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../modules/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
 
 function App() {
 
-  //fetch activities from the db
+  
   const [activities, setActivities] = useState<Activity[]>([]) 
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
@@ -35,6 +36,16 @@ function App() {
     setEditMode(false);
   }
 
+  function handleCreateOrEditActivity(activity: Activity){
+    activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity]) :
+    setActivities([...activities, {...activity, id: uuid()}]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id: string){
+    setActivities([...activities.filter(x => x.id !== id)])
+  }
 
 
   return (
@@ -49,7 +60,9 @@ function App() {
           cancelSelectActivity={handleCancelSelectActivity}
           editMode={editMode}
           openForm={handleFormOpen}
-          closeForm={handleFormClose}          
+          closeForm={handleFormClose}    
+          createOrEdit={handleCreateOrEditActivity}     
+          deleteActivity={handleDeleteActivity}
           />
       </Container>
       
